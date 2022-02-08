5b5ffc802f5bea47aa19d36952c7b1214526aa61
huguettekabemba <kab21012@byui.edu>
2022-02-09 00:01:58 +0200

    commit

--- word_list.py
+++ word_list.py
@@ -1,3 +1,8 @@
 #this file will handle word display
 #the word list
 #and letters guessed correctly and incorrectly
+import random
+words_list = ["labbery", "snoopers", "stabbers", "subcells", "tachists","tacklers",
+"subfloor", "swabbers", "tabbises", "taborers", "tachisms", "tachiste",
+"tackless", "taillamp", "tailless", "tallness", "teaberry"]
+words = random.choice(words_list).upper()
